5e58fe37f901fab0e7ef58aed5f3587e70f1b1fc
Lixiaolong <1076187989@qq.com>
2019-04-17 21:03:20 +0800

    Signed-off-by: Lixiaolong <1076187989@qq.com>

---
+++ FatigueDetection_1.0/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace FatigueDetection_1._0
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.imageBox = new Emgu.CV.UI.ImageBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.mouthChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.eyeChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.glassCheckBox = new System.Windows.Forms.CheckBox();
+            this.quitButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mouthChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eyeChart)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageBox
+            // 
+            this.imageBox.Location = new System.Drawing.Point(12, 20);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(320, 240);
+            this.imageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox.TabIndex = 2;
+            this.imageBox.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.imageBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(344, 265);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "实时视频";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.mouthChart);
+            this.groupBox2.Controls.Add(this.eyeChart);
+            this.groupBox2.Location = new System.Drawing.Point(370, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(300, 265);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "眼睛宽高比";
+            // 
+            // mouthChart
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.mouthChart.ChartAreas.Add(chartArea7);
+            legend7.Enabled = false;
+            legend7.Name = "Legend1";
+            this.mouthChart.Legends.Add(legend7);
+            this.mouthChart.Location = new System.Drawing.Point(6, 138);
+            this.mouthChart.Name = "mouthChart";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.Legend = "Legend1";
+            series7.Name = "mouth";
+            this.mouthChart.Series.Add(series7);
+            this.mouthChart.Size = new System.Drawing.Size(288, 112);
+            this.mouthChart.TabIndex = 1;
+            this.mouthChart.Text = "chart1";
+            // 
+            // eyeChart
+            // 
+            this.eyeChart.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea8.AxisX.IsLabelAutoFit = false;
+            chartArea8.AxisY.IsLabelAutoFit = false;
+            chartArea8.AxisY2.Crossing = -1.7976931348623157E+308D;
+            chartArea8.AxisY2.IsLabelAutoFit = false;
+            chartArea8.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.Top;
+            chartArea8.Name = "ChartArea1";
+            chartArea8.Position.Auto = false;
+            chartArea8.Position.Height = 94F;
+            chartArea8.Position.Width = 100F;
+            chartArea8.Position.Y = 3F;
+            this.eyeChart.ChartAreas.Add(chartArea8);
+            legend8.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            legend8.Enabled = false;
+            legend8.IsTextAutoFit = false;
+            legend8.Name = "Legend1";
+            this.eyeChart.Legends.Add(legend8);
+            this.eyeChart.Location = new System.Drawing.Point(6, 20);
+            this.eyeChart.Name = "eyeChart";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.Legend = "Legend1";
+            series8.Name = "eye";
+            this.eyeChart.Series.Add(series8);
+            this.eyeChart.Size = new System.Drawing.Size(288, 112);
+            this.eyeChart.TabIndex = 0;
+            this.eyeChart.Text = "chart1";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.glassCheckBox);
+            this.groupBox3.Controls.Add(this.quitButton);
+            this.groupBox3.Controls.Add(this.startButton);
+            this.groupBox3.Location = new System.Drawing.Point(12, 299);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(344, 87);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "系统设置";
+            // 
+            // glassCheckBox
+            // 
+            this.glassCheckBox.AutoSize = true;
+            this.glassCheckBox.Location = new System.Drawing.Point(20, 40);
+            this.glassCheckBox.Name = "glassCheckBox";
+            this.glassCheckBox.Size = new System.Drawing.Size(72, 16);
+            this.glassCheckBox.TabIndex = 2;
+            this.glassCheckBox.Text = "佩戴眼镜";
+            this.glassCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // quitButton
+            // 
+            this.quitButton.Location = new System.Drawing.Point(248, 36);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(75, 23);
+            this.quitButton.TabIndex = 1;
+            this.quitButton.Text = "安全退出";
+            this.quitButton.UseVisualStyleBackColor = true;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(127, 36);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "启动系统";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Location = new System.Drawing.Point(370, 299);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(300, 87);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "提醒";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(41, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(38, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 412);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "mainForm";
+            this.Text = "驾驶行为安全检测系统";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mouthChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eyeChart)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button quitButton;
+        private System.Windows.Forms.CheckBox glassCheckBox;
+        private System.Windows.Forms.DataVisualization.Charting.Chart eyeChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart mouthChart;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FatigueDetection_1.0/Form1.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms.DataVisualization.Charting;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+
+using System.Data.SqlClient;
+
+
+namespace FatigueDetection_1._0
+{   
+    
+    public partial class mainForm : Form
+    {
+        //=============全局变量start========================
+
+        private VideoCapture capture = null;
+        public Mat frame;
+        public struct faceDetection
+        {
+            public Mat srcImage;
+            public Mat faceRoi;
+            public int coor_x;
+            public int coor_y;
+            public bool FLAG;
+        };
+        public struct eyeDetection
+        {
+            public Mat faceImage;
+            public Mat eyeRoi;
+            public bool FLAG;
+        };
+        public struct EllipseEye
+        {
+            public Mat res;
+            public float width;
+            public float height;
+            public bool FLAG;
+        };
+
+        public Mat resShowImage;
+        public Mat eyeROI;
+        public bool eyeFLAG;
+        public Mat faceRectImage;
+        public Mat faceROI;
+        public bool faceFLAG;
+        public int coor_x;
+        public int coor_y;
+        public bool flag;   // 是否戴眼镜，true为不带，false为带
+
+        public int nHeadPosture = 0;
+        public float PERCLOS;
+        public bool ellipseFLAG;
+
+        public float eyeOpenDegree;
+        public float mouthOpenDegree;
+
+        public CascadeClassifier cascadeFace;
+        public CascadeClassifier cascadeEye;
+        public CascadeClassifier cascadeEyeWithGlasses;
+
+        public const string cascadeNameFace = "haarcascade_frontalface_alt2.xml";
+        public const string cascadeNameEyeWithGlasses = "haarcascade_eye_tree_eyeglasses.xml";
+        public const string cascadeNameEyeNoGlasses = "haarcascade_eye.xml";
+
+        Timer chartTimer = new Timer();
+        List<float> eye = new List<float>(new float[20]);
+        TimeSpan ts;
+
+        //===================全局变量end=======================================================
+
+        public mainForm()
+        {
+            cascadeFace = new CascadeClassifier(cascadeNameFace);
+            cascadeEye = new CascadeClassifier(cascadeNameEyeNoGlasses);
+            cascadeEyeWithGlasses = new CascadeClassifier(cascadeNameEyeWithGlasses);
+            
+            InitializeComponent();
+            
+
+        }
+        
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            InitChart();
+            capture = new VideoCapture();
+            capture.SetCaptureProperty(CapProp.FrameHeight, 240);
+            capture.SetCaptureProperty(CapProp.FrameWidth, 320);
+
+            capture.ImageGrabbed += ProcessFrame;
+
+            frame = new Mat();
+            if (capture != null)
+                capture.Start();         
+        }
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            DateTime start = System.DateTime.Now;
+
+            if (capture != null && capture.Ptr != IntPtr.Zero)
+            {
+                
+                capture.Retrieve(frame, 0);
+                resShowImage = process(frame, flag);
+                imageBox.Image = resShowImage;      //imageBox显示控件
+            }
+
+            DateTime end = System.DateTime.Now;
+            ts = end.Subtract(start);
+
+            textBox1.Text = Convert.ToInt32(ts.TotalMilliseconds).ToString();   // 算法速度
+        }
+
+        public Mat process(Mat image, bool flag)
+        {
+            // 人脸检测 START...
+
+            flag = glassCheckBox.Checked ? false : true;
+
+            faceDetection face = new faceDetection();
+
+            face = facedetection(image, cascadeFace);
+            faceFLAG = face.FLAG;
+            faceRectImage = face.srcImage;
+            faceROI = face.faceRoi;
+            coor_x = face.coor_x;
+            coor_y = face.coor_y;
+
+            // 人脸检测 END...
+            // 眼睛检测 START...
+
+            if (faceFLAG)
+            {
+                if (flag)          // 不戴眼镜
+                {
+                    eyeDetection eye = eyedetection(faceROI, cascadeEye, faceRectImage, coor_x, coor_y);
+                    resShowImage = eye.faceImage;
+                    eyeROI = eye.eyeRoi;
+                    eyeFLAG = eye.FLAG;
+                }
+                else              // 戴眼镜
+                {
+                    eyeDetection eye = eyedetection(faceROI, cascadeEyeWithGlasses, faceRectImage, coor_x, coor_y);
+                    resShowImage = eye.faceImage;
+                    eyeROI = eye.eyeRoi;
+                    eyeFLAG = eye.FLAG;
+                }
+
+                // 眼睛检测 END...
+                // 椭圆拟合 START...
+
+                if (eyeFLAG)    // 检测到眼睛
+                {
+                    CvInvoke.Resize(eyeROI, eyeROI, new Size(24, 24));
+                    EllipseEye e = eyeEllipse(eyeROI);
+                    if (e.FLAG)        // 椭圆拟合完成
+                    {
+                        eyeOpenDegree = e.width / e.height;
+                        
+                        if (eye.Count == 20)
+                        {
+                            //eye.ToArray();
+                            eye.Reverse();
+                            eye.RemoveAt(19);
+                            eye.Reverse();
+                            
+                        }
+                        eye.Add(eyeOpenDegree);
+                        
+                    }
+                    else        // 椭圆拟合未完成
+                    {
+                        eyeOpenDegree = 0.1F;
+                        
+                        if (eye.Count == 20)
+                        {
+                            //eye.ToArray();
+                            eye.Reverse();
+                            eye.RemoveAt(19);
+                            eye.Reverse();
+                        }
+                            
+                        eye.Add(eyeOpenDegree);
+                        
+                    }
+                }
+                else     // 未检测到眼睛
+                {
+                    eyeOpenDegree = 0.1F;
+                    
+                    if (eye.Count == 20)
+                    {
+                        //eye.ToArray();
+                        eye.Reverse();
+                        eye.RemoveAt(19);
+                        eye.Reverse();
+                    }
+                    eye.Add(eyeOpenDegree);
+                    
+
+                }
+            }
+            else   // 未检测到人脸
+            {
+                nHeadPosture++;
+            }
+
+            return resShowImage;
+        }
+
+/////////////////////////////////////////////////////////////////////////////////////
+        public faceDetection facedetection(Mat image, CascadeClassifier cascade)
+        {
+            faceDetection face = new faceDetection();
+            Mat tmp = new Mat();
+            image.CopyTo(tmp);
+            Mat grayImage = new Mat();
+            List<Rectangle> faces = new List<Rectangle>();
+            CvInvoke.CvtColor(tmp, grayImage, ColorConversion.Bgr2Gray);
+            CvInvoke.EqualizeHist(grayImage, grayImage);     // 均衡化
+
+            Rectangle[] facedetected = cascade.DetectMultiScale(grayImage, 1.1, 3, new Size(80, 80));
+            faces.AddRange(facedetected);
+            int rect_x, rect_y, rect_width, rect_height;
+
+            if (faces.Count > 0)
+            {       
+                rect_x = faces[0].X;
+                rect_y = faces[0].Y;
+                rect_width = faces[0].Width;
+                rect_height = faces[0].Height;
+
+                CvInvoke.Rectangle(tmp, faces[0], new Bgr(Color.Red).MCvScalar, 1);
+
+                face.faceRoi = new Mat(image, faces[0]);
+                face.coor_x = rect_x;
+                face.coor_y = rect_y;
+                face.srcImage = tmp;
+                face.FLAG = true;
+
+                return face;              
+            }
+            else
+            {
+                face.srcImage = tmp;
+                face.FLAG = false;
+
+                return face;
+            }
+        }
+/////////////////////////////////////////////////////////////////////////////////////////
+        public eyeDetection eyedetection(Mat image, CascadeClassifier cascade, Mat faceRectImage, int coor_x, int coor_y)
+        {
+            eyeDetection eye = new eyeDetection();
+            Mat tmp = new Mat();
+            image.CopyTo(tmp);
+            Mat grayImage = new Mat();
+            List<Rectangle> eyes = new List<Rectangle>();
+
+            CvInvoke.CvtColor(tmp, grayImage, ColorConversion.Bgr2Gray);
+            CvInvoke.EqualizeHist(grayImage, grayImage);     // 均衡化
+
+            Rectangle[] eyedetected = cascade.DetectMultiScale(grayImage, 1.1, 3, new Size(20, 20));
+            eyes.AddRange(eyedetected);
+
+            int rect_x, rect_y, rect_width, rect_height;
+
+            if (eyes.Count > 0)
+            {
+                rect_x = eyes[0].X;
+                rect_y = eyes[0].Y;
+                rect_width = eyes[0].Width;
+                rect_height = eyes[0].Height;
+
+                CvInvoke.Rectangle(faceRectImage, new Rectangle(rect_x + coor_x, rect_y + coor_y, rect_width, rect_height), new Bgr(Color.Blue).MCvScalar, 1);
+
+                eye.eyeRoi = new Mat(image, eyes[0]);
+                eye.faceImage = faceRectImage;
+                eye.FLAG = true;
+
+                return eye;
+            }
+            else
+            {
+                eye.faceImage = faceRectImage;
+                eye.FLAG = false;
+                return eye;
+            }
+        }
+        /////////////////////////////////////////////////////////////////////////
+
+        public EllipseEye eyeEllipse(Mat image)
+        {
+            Mat tmp = new Mat();
+            image.CopyTo(tmp);
+
+            EllipseEye e = new EllipseEye();
+            Mat hsv = new Mat();
+            CvInvoke.CvtColor(tmp, hsv, ColorConversion.Bgr2Hsv);
+
+            VectorOfMat m = new VectorOfMat();
+            CvInvoke.Split(hsv, m);
+
+            OutputArray channels = m.GetOutputArray();
+            Mat H_channel = new Mat();
+            H_channel = channels.GetMat(0);
+
+            Mat dst = new Mat();
+            CvInvoke.Threshold(H_channel, dst, 0, 255, ThresholdType.Otsu);
+
+            Mat ele = CvInvoke.GetStructuringElement(ElementShape.Ellipse, new Size(3, 3), new Point(1, 1));
+            CvInvoke.Erode(dst, dst, ele, new Point(1, 1), 1, BorderType.Default, new MCvScalar(0));
+
+            CvInvoke.BitwiseNot(dst, dst);  // 反色
+
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            
+            CvInvoke.FindContours(dst, contours, null, RetrType.List, ChainApproxMethod.ChainApproxNone);
+            
+            for (var i = 0; i < contours.Size; i++)
+            {
+                var count = contours[i].Size;
+                if (count < 6)
+                    continue;
+              
+                RotatedRect box = CvInvoke.FitEllipse(contours[i]);
+                var area = box.Size.Height * box.Size.Width;
+
+                if (area >= 192 || box.Size.Height == box.Size.Width)
+                {
+                    continue;
+                }
+
+                if (!box.Size.IsEmpty)
+                {
+                    e.width = box.Size.Width;
+                    e.height = box.Size.Height;
+
+                    CvInvoke.Ellipse(tmp, box, new MCvScalar(0, 0, 255), 2);
+                    e.res = tmp;
+                    e.FLAG = true;
+                    return e;
+                }
+                else
+                {
+                    e.FLAG = false;
+                    return e;
+                }
+
+            }
+            e.FLAG = false;
+            return e;
+        }
+        /////////////////////////////////////////////////////////////////////////////
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+            capture.Stop();
+            Application.Exit();
+        }
+       
+        private void InitChart()
+        {
+            eyeChart.ChartAreas["ChartArea1"].AxisY.Minimum = 0;
+            eyeChart.ChartAreas["ChartArea1"].AxisY.Maximum = 1;
+
+            int x = Convert.ToInt32(ts.TotalMilliseconds);
+            if (x == 0) x = 1;
+            chartTimer.Interval = x;
+            chartTimer.Tick += Timer_Tick;
+            chartTimer.Start();
+        }
+               
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            Series series = eyeChart.Series[0];
+            series.Points.DataBindY(eye);          
+        }
+
+
+
+
+        
+
+    }
+}
